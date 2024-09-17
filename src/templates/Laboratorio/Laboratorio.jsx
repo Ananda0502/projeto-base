25747e0c70e7694aa995a6b3046468e746bdc5fa
Ananda0502 <marciaevelly7@gmail.com>
2024-09-17 08:33:00 -0300

    cor

--- src/templates/Laboratorio/Laboratorio.jsx
+++ src/templates/Laboratorio/Laboratorio.jsx
@@ -17,11 +17,11 @@ const Laboratorio = () => {
                <section className="m-2 p-2 shadow-lg">
                     <div className="d-flex justify-content-around">
                         <Link to={'/laboratorionovo'} 
-                            className="btn btn-lg btn-primary">
+                            className="btn btn-lg btn-primary" id="novo">
                             Novo Laboratório
                         </Link>
                         <Link to={'/laboratorioslista'} 
-                            className="btn btn-lg btn-warning">
+                            className="btn btn-lg btn-warning" id="lista">
                             Lista de Laboratórios
                         </Link>
                     </div>
