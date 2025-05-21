0321fad8ddbb509cfedc6e1e64ff79f4fbdeb6b9
sofi1205 <mendezsofia735@gmail.com>
2025-05-21 10:13:24 -0300

    cadastro crud funcionando uuuuuuuuuuuuuuuuuuuuuuuuhul galerinha do mal Adriano arrasa

--- src/services/TecnicoService.js
+++ src/services/TecnicoService.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const API_URL = "http://localhost:8080/tecnicos"; // ajuste se seu backend estiver rodando em outra porta
 
 const cadastrarTecnico = (tecnico) => {
+  alert(JSON.stringify(tecnico))
   return axios.post(API_URL, tecnico);
 };
 
