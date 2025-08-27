46e68e8d28008cc8158ff9643f650b08f2c56014
Ananda0502 <marciaevelly7@gmail.com>
2025-08-27 10:29:50 -0300

    laboratorio meio que funcionando kkkk

--- src/services/LaboratorioService.js
+++ src/services/LaboratorioService.js
@@ -2,45 +2,38 @@ import http from '../common/http-common';
 
 const API_URL = "http://localhost:8080/localidade";
 
-// Método para buscar todos os laboratórios
+// Buscar todos os laboratórios
 const getAllLaboratorios = () => {
     return http.mainInstance.get(API_URL);
 };
 
-// Método para buscar um laboratório por ID
+// Buscar laboratório por ID
 const getById = (id) => {
-    return http.mainInstance.get(`${API_URL}/findById/${id}`);
+    return http.mainInstance.get(`${API_URL}/${id}`);
 };
 
-// Método para salvar um novo laboratório
+// Salvar novo laboratório
 const saveLaboratorio = (laboratorio) => {
+    // laboratorio deve ter a estrutura { nome, andar, sala, ativo? }
     return http.mainInstance.post(API_URL, laboratorio);
-}; 
-
-// Verificar duplicidade
-const verificarDuplicidade = (sala, andar) => {
-    return http.mainInstance.get(`${API_URL}/verificarDuplicidade?sala=${sala}&andar=${andar}`);
 };
 
-// Método para atualizar um laboratório existente
+// Atualizar laboratório
 const updateLaboratorio = (id, laboratorio) => {
     return http.mainInstance.put(`${API_URL}/${id}`, laboratorio);
 };
 
-// Método para excluir um laboratório por ID
+// Excluir laboratório
 const deleteLaboratorio = (id) => {
-    console.log(`Deletando laboratório com ID: ${id}`);
     return http.mainInstance.delete(`${API_URL}/${id}`);
 };
 
-// Exportação do serviço
 const LaboratorioService = {
     getAllLaboratorios,
     getById,
     saveLaboratorio,
     update: updateLaboratorio,
-    verificarDuplicidade,
-    deleteLaboratorio  // Aqui, exportando a função corretamente
+    deleteLaboratorio,
 };
 
 export default LaboratorioService;
